acd716fd932782a366c69ed2965364e55e14dcad
Mostafa <mostafa.safaeipour1@gmail.com>
2021-09-27 11:41:22 +0330

    change test node

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const Scooter = require('./lib/scooter').Scooter
 const scooterCommands = new Scooter(aedes, redisClient)
 
 const MQTTport = 1883
-var serverRes = {}
 
 function getFirstItemOfString(response) {
     const splittedItem = response.split('=')
@@ -41,14 +40,6 @@ redisClient.on('connect', function () {
     console.log('Redis Connected');
 })
 
-/*
-
-app.post('/testapi', function (req, res) {
-    res.json({ "Status": "OK", "List": [{ "ScooterID": "A1111", "Available": "1", "Longitude": "0", "Latitude": "0", "Battery": "49", "Lock": "0", "Velocity": "0" }] });
-})
-
-*/
-
 app.post('/TurnLightOn', function (request, response) {
     scooterCommands.turnOnTheLight(request.body['ScooterID'])
     response.json({ 'Status': 'Sucess' })
@@ -69,130 +60,12 @@ app.post('/UnLock', function (request, response) {
     response.json({ 'Status': 'Sucess' })
 })
 
-app.post('/StartTrip', function (req, res) {
-    redisClient.sismember('Scooters', req.body['ScooterID'], function (err, object) {
-        redisClient.hget(req.body['ScooterID'], 'Available', function (err, hgetobject) {
-            if (object === 0 || hgetobject.toString() == '0')
-                res.json({ 'Status': 'NotAvailable' })
-            else {
-                redisClient.hmget(req.body['ScooterID'], ['Longitude', 'Latitude', 'Available'], function (errget, objectget) {
-                    redisClient.hmset(req.body['PhoneNumber'], 'ScooterID', req.body['ScooterID'], 'SLongitude', objectget[0], 'SLatitude', objectget[1], 'STime', getFullDate())
-                    redisClient.hmset(req.body['ScooterID'], 'Available', 0)
-                    UnLockScooter(req.body['ScooterID'])
-                    res.json({ 'Status': 'Sucess' })
-                })
-            }
-        })
-    })
-})
-
-app.post('/EndTrip', function (req, res) {
-    redisClient.sismember('Scooters', req.body['ScooterID'], function (err, object) {
-        var checkavailable
-        redisClient.hget(req.body["ScooterID"], 'Available', function (err, hgetobject) {
-            if (object === 0 || hgetobject.toString() == '1')
-                res.json({ "Status": "Error" })
-            else {
-                redisClient.hmget(req.body["ScooterID"], ['Longitude', 'Latitude'], function (errget, objectgetm) {
-                    redisClient.hgetall(req.body["PhoneNumber"], function (errgetall, objectgetall) {
-                        //Send Data To add(User/Scooter)Usage.php
-                        var jsontousage = {
-                            'PhoneNumber': req.body["PhoneNumber"],
-                            'ScooterID': req.body["ScooterID"],
-                            'STime': objectgetall['STime'],
-                            'ETime': getFullDate(),
-                            'SLongitude': objectgetall['SLongitude'],
-                            'SLatitude': objectgetall['SLatitude'],
-                            'ELongitude': objectgetm[0],
-                            'ELatitude': objectgetm[1]
-                        }
-                        fetch('http://127.0.0.1/usage/addUserUsage.php', {
-                            method: 'post',
-                            body: JSON.stringify(jsontousage),
-                            Headers: { 'Content-Type': 'application/json' }
-                        }).then(UserUsageRes => UserUsageRes.json()).then(UserUsageResjson => console.log(UserUsageResjson))
-
-                        fetch('http://127.0.0.1/usage/addScooterUsage.php', {
-                            method: 'post',
-                            body: JSON.stringify(jsontousage),
-                            Headers: { 'Content-Type': 'application/json' }
-                        }).then(ScooterUsageRes => ScooterUsageRes.json()).then(ScooterUsageResjson => console.log(ScooterUsageResjson))
-
-                        redisClient.del(req.body["PhoneNumber"])
-                        redisClient.hmset(req.body["ScooterID"], 'Available', 1)
-                        LockScooter(req.body["ScooterID"])
-                        res.json({ "Status": "Sucess" })
-                    })
-                })
-            }
-        })
-    })
-})
-
-
-app.post('/ScooterDetail', function (req, res) {
-    var ScooterDetailObject, detaillength
-    var inde = 0
-    redisClient.smembers('Scooters', function (err, object) {
-        ScooterDetailObject = Array.from(object)
-        if (ScooterDetailObject.length == 0) {
-            res.json({ "Status": "OK", "List": [] })
-            return
-        }
-        serverRes = {}
-        serverRes["Status"] = "OK"
-        serverRes["List"] = []
-        detaillength = ScooterDetailObject.length
-        for (var i = 0; i < ScooterDetailObject.length; i++) {
-            redisClient.hgetall(ScooterDetailObject[i], function (error, getres) {
-                inde++
-                if (getres["Available"] == 1) {
-                    serverRes["List"].push(getres)
-                }
-
-                if (detaillength === inde)
-                    res.json(serverRes)
-            })
-        }
-    })
-
-})
-
-
-app.post('/OneScooterDetail', function (req, res) {
-    serverRes = {}
-    serverRes["Status"] = "OK"
-    serverRes["List"] = []
-    redisClient.sismember('Scooters', req.body["ScooterID"], function (err, object) {
-        redisClient.hgetall(req.body["ScooterID"], function (error, getres) {
-            if (object === 1 && getres["Available"] == 1) {
-                serverRes["List"].push(getres)
-                res.json(serverRes)
-            } else
-                res.json({ "Status": "NotAvailable" })
-        })
-    })
-})
-
-app.post('/CheckScooterAvailable', function (req, res) {
-    redisClient.sismember('Scooters', req.body["ScooterID"], function (err, object) {
-        redisClient.hget(req.body["ScooterID"], 'Available', function (err, hgetobject) {
-            if (object === 0 || hgetobject.toString() == '0')
-                res.json({ "Status": "NotAvailable" })
-            else
-                res.json({ "Status": "Available" });
-        })
-    })
-})
-
 app.listen(8080)
-
 
 
 /*
 ////////////////MQTT Part
 */
-
 
 aedes.authenticate = function (client, username, password, callback) {
     
@@ -201,11 +74,6 @@ aedes.authenticate = function (client, username, password, callback) {
     //    return
     //}
 
-    //dbo.collection("scooterdetail").findOne({token:password.toString(),id:username},function(err, result) {
-    // if (result === null || result.length === 0 || err){
-    //  callback(null,false)
-    //}
-    //})
     callback(null, true)
 }
 
@@ -215,6 +83,7 @@ server.listen(MQTTport, function () {
 
 aedes.on('client', function (client) {
     console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    
     redisClient.hmset(client.id, 'ScooterID', client.id, 'Available', 1)
     redisClient.sadd('Scooters', client.id)
     scooterCommands.lockScooter(client.id)
@@ -222,6 +91,7 @@ aedes.on('client', function (client) {
 
 aedes.on('clientDisconnect', function (client) {
     console.log('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    
     redisClient.del(client.id)
     redisClient.srem('Scooters', client.id)
 })
--- test/test.js
+++ test/test.js
@@ -4,25 +4,68 @@ const axios = require('axios')
 const CLIENT_ID = 'A1111'
 const SERVER_URI = 'server-node'
 
-const mqttClient = mqtt.connect(`mqtt://${SERVER_URI}`, {
-    clientId : CLIENT_ID
-})
+let mqttClient
 
-mqttClient.on('connect', function() {
-    console.log('Connected to server!')
+const subscribeMqttPromise = new Promise((resolve, reject) => {
+   
+    if (typeof mqttClient == 'undefined')
+        reject('mqttClient not defined')
+
     mqttClient.subscribe(`/scooter/admin/${CLIENT_ID}`, function(err) {
         if (!err)
-            console.log(`Subscribed to /scooter/admin/${CLIENT_ID}`)
+            resolve('Subscribed')
+        else
+            reject('Cannot subscribe')
     })
 })
 
 
-mqttClient.on('message', function(topic, message) {
-    console.log(message.toString())
+
+beforeEach( async () => {
+    
+    await new Promise((resolve, reject) => {
+
+        mqttClient = mqtt.connect(`mqtt://${SERVER_URI}`, {
+            clientId : CLIENT_ID
+        })
+
+        mqttClient.on('connect', function() {
+            resolve()
+        })
+    }) 
+
 })
 
 
-//For finding errors
-mqttClient.on('error', function(err) {
-    console.log(err)
+test('Testing Lock Scooter', () => {
+
+    return subscribeMqttPromise.then(res => {
+        
+        return new Promise((resolve, reject) => {
+            mqttClient.publish(`/scooter/${CLIENT_ID}`, 
+                'id=A1111&longtiude=12&latitude=12&battery=50&lock=0&velocity=10', 
+                function(err) {
+
+                if (!err)
+                    resolve('The message has been sent')
+                else
+                    reject('Error in sending the message')
+
+            })   
+        })
+
+    }).then(res => { 
+        return axios.post(`http://${SERVER_URI}/Lock`, {
+            ScooterID : 'A1111' 
+        })
+    }).then(response => {
+        
+        resObj = JSON.parse(response)
+        expect(resObj['Status']).toEqual('Sucess')
+
+    }).catch(rej => {
+        throw new Error(rej)
+    })
+
 })
+
